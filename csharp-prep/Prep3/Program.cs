d15284343155c9f2b353b3c1c86f5b487bb76c84
epeters-byui <pemy1981@gmail.com>
2023-07-18 02:32:22 +0000

    added commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   // For part 3 we used random number
+         Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+       // For part 1 and 2 user call fro number..
+       // Console.Write("What is the magic number? ");
+        //int userNumber = int.Parse(Console.ReadLine());
+
+        if (userNumber < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (userNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+        while (userNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
