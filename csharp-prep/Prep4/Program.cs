e2072aca4e051ffce94ab8014c6f32483b808b27
epeters-byui <pemy1981@gmail.com>
2023-07-18 03:30:30 +0000

    Added commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // We added parts 1, 2 and 3 at the end of code/ program with.
+         List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int input;
+        do
+        { // We used ToInt32 fro the string with 32-bit integr value.
+            Console.Write("Enter number: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+        int sum = 0;
+        int max = int.MinValue;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        double average = (double)sum / numbers.Count;
+
+        // The programm print the user input from the below code.
+
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: " + max);
     }
 }
